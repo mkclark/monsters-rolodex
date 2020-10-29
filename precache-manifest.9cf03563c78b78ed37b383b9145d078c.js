c8610d1887e632119badd377978690035a931349
Morgan <morgankclark@gmail.com>
2020-10-28 20:53:16 -0700

    Updates

---
+++ precache-manifest.9cf03563c78b78ed37b383b9145d078c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5446c053b85eb90ce31b2d0d74dbffc1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "43f2ea418aa2e92d2ab3",
+    "url": "/monsters-rolodex/static/css/main.4abb063c.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43f2ea418aa2e92d2ab3",
+    "url": "/monsters-rolodex/static/js/main.d78bf800.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
